19c5158908d94ca0703c04f2719e84c24308d49d
luisfelipekai <luisfelipekai@gmail.com>
2017-03-04 10:28:24 -0300

    Create SQlite db. Add Model classes to Admin site.

---
+++ api/migrations/0001_initial.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('first_name', models.CharField(max_length=150)),
+                ('last_name', models.CharField(max_length=150)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Book',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=100)),
+                ('isbn', models.CharField(max_length=15)),
+                ('publish_year', models.CharField(max_length=4)),
+                ('author', models.ForeignKey(to='api.Author', related_name='books')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Publisher',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+            ],
+        ),
+        migrations.AddField(
+            model_name='book',
+            name='publisher',
+            field=models.ForeignKey(to='api.Publisher', related_name='books'),
+        ),
+    ]

a9d8f7a0729afe3a43357c9c7098253dd08512cd
luisfelipekai <luisfelipekai@gmail.com>
2017-03-04 13:46:50 -0300

    Add Materialize elements to the Page (Footer, Book form, Navbar).

--- static/js/controllers/AllBooksController.js
+++ static/js/controllers/AllBooksController.js
@@ -7,5 +7,9 @@
 
     function AllBooksController($scope) {
         $scope.message = "All Books Controller";
+        $scope.select = {
+            value: "Option1",
+            choices: ["Option1", "I'm an option", "This is materialize", "No, this is Patrick."]
+        };
     }
 }());

924299d9e247643f4ae84b3249538ad00a8004d5
luisfelipekai <luisfelipekai@gmail.com>
2017-03-08 11:21:50 -0300

    Populate Author and Publisher DropDownList using the dataService.

--- static/js/controllers/AllBooksController.js
+++ static/js/controllers/AllBooksController.js
@@ -3,11 +3,25 @@
  */
 (function(){
     angular.module('app')
-        .controller('AllBooksController', ['$scope', 'books', AllBooksController]);
+        .controller('AllBooksController', ['$scope', 'books', 'dataService', AllBooksController]);
 
-    function AllBooksController($scope, books) {
+    function AllBooksController($scope, books, dataService) {
         $scope.showForm = false;
         $scope.message = "All Books";
         $scope.books = books;
+        $scope.book = {};
+        $scope.authors = {};
+        $scope.publishers = {};
+        $scope.add = function(){
+
+        };
+        dataService.getAllAuthors().then(function(response){
+            $scope.book.author = response[0];
+            $scope.authors = response;
+        });
+        dataService.getAllPublishers().then(function(response){
+            $scope.book.publisher  = response[0];
+            $scope.publishers = response;
+        });
     }
 }());
